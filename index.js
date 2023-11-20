85c6d6da11400c4ef09e723be4b7416a3c839caf
molly <molly.salzberger@gmail.com>
2023-11-19 21:19:21 -0600

    not only did I forget async, i forgot the GIT INIT

---
+++ index.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const SVG = require('./lib/svg')
+const {Circle, Square, Triangle} = require('./lib/shapes')
+
+function init(){
+    //inquirer prompts. remember to name & prompt message
+            //choose a shape type: list, choices: []
+        //shape color. type: input
+        //text (three letters). type: input
+        // text color. type: input
+    inquirer.prompt([
+        {
+            type: list,
+            name: shapeChoice,
+            message: 'Choose a shape for your logo badge',
+            choices: [
+                "Circle",
+                "Square",
+                "Triangle"
+            ]
+        },
+        {
+            type: list,
+            name: shapeColor,
+            message: 'Choose a background color',
+            choices: [
+                "red",
+                "blue",
+                "yellow",
+                "green",
+                "purple",
+                "orange"
+            ]
+        },
+        {
+            type: input,
+            name: logoText,
+            message: 'Choose up to three letters for your logo text',
+        },
+        {
+            type: list,
+            name: textColor,
+            message: 'Choose a text color',
+            choices: [
+                "white",
+                "black",
+                "pink"
+            ]
+        }
+    ]);
+
+    const userShapeChoice = userDesign.shapeChoice;
+    const shapeColorChoice = userDesign.shapeColor;
+    const textChoice = userDesign.logoText;
+    const textColorChoice = userDesign.textColor;
+
+    const userLogo = new SVG(userShapeChoice, shapeColorChoice, textChoice, textColorChoice);
+    userLogo.writeText();
+    userLogo.drawShape();
+    finalProduct = userLogo.allTogether();
+    fs.writeFile('logo.svg', finalProduct);
+
+}
+
+init();
