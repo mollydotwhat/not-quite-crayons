74340033fa1f57fe345047ceb74bfe664d7c1b45
molly <molly.salzberger@gmail.com>
2024-05-17 11:58:42 -0500

    outlined a test, not sure about function nesting here

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,1 +1,12 @@
 const {Circle, Square, Triangle} = require('./lib/shapes');
+const SVG = require('./lib/svg');
+
+describe('Circle', () => {
+    describe('render', () => {
+        it('should produce circle HTML', () => {
+            const aCircle = new Circle();
+            //do I need to include color to test this properly
+            // & if so should test SVG functions
+        })
+    })
+});
