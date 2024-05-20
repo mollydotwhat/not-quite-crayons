2cc0eea9782a89a292e28d061a6c25e03e16082a
molly <molly.salzberger@gmail.com>
2024-05-20 11:11:52 -0500

    tried to test result HTML w/shape & color together & test returned color as object. testing color property by itself worked.

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,13 +1,17 @@
-const {Circle, Square, Triangle} = require('./lib/shapes');
-const SVG = require('./lib/svg');
+const {Circle, Square, Triangle} = require('../lib/shapes');
+//const SVG = require('../lib/svg');
 
 //could also make overall shapes test w/each nested within, but i don't love nesting functions for miles.
 describe('Circle', () => {
     describe('render', () => {
         it('should produce circle HTML', () => {
-            const aCircle = new Circle();
-            //do I need to include color to test this properly
-            // & if so should test SVG functions
+            const greenCircle = new Circle("green","circle");
+
+            // expect(greenCircle).toEqual(`<circle cx="150" cy="100" r="80" fill="green" />`);
+            //green comes out as an object? not sure why yet. template literal didn't help.
+
+            //test to isolate expecting green
+            expect(greenCircle.color == "green")
         })
     })
 });
