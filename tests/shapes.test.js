b7a0099460285654fd24a698676d9fddfff00867
molly <molly.salzberger@gmail.com>
2024-05-20 10:53:21 -0500

    test for green circle.

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,6 +1,7 @@
 const {Circle, Square, Triangle} = require('./lib/shapes');
 const SVG = require('./lib/svg');
 
+//could also make overall shapes test w/each nested within, but i don't love nesting functions for miles.
 describe('Circle', () => {
     describe('render', () => {
         it('should produce circle HTML', () => {
